5c6957f824c27efdc8d74afa9205d712dbe11a1e
Pavlo Kandiak <kandiakps@gmail.com>
2018-10-03 18:32:29 +0300

    week_2_lesson_2_pavlo_kandiak fix bug in task3

--- week2/lesson2-intro-to-functional/homework/PavloKandiak/utils/task3.py
+++ week2/lesson2-intro-to-functional/homework/PavloKandiak/utils/task3.py
@@ -1,3 +1,4 @@
-def name_uppercase(data: list):
+def add_load(data: list):
 	for i in data:
-		i['name'] = i['name'].upper()
+		l = i['age'] * 100 / 200
+		i['load'] = int(l) if l.is_integer() else l
